095af125054ee76ec1840b60e54c6bc9b726d76c
sajjadabd <mynameissajjad@gmail.com>
2023-08-24 18:44:07 +0330

    add navigation links in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,14 @@
-
+import { Link } from 'react-router-dom';
 
 export default function Header () {
 	return (
-			<div>Header</div>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
+      </nav>
+    </header>
 	)
 }
