183db7b9b35765a5afdbd0e33156457ae803cb3a
sajjadabd <mynameissajjad@gmail.com>
2023-08-26 00:51:26 +0330

    add divider for Home page title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-
+import { Divider } from 'antd';
 
 export default function Home () {
   return (
-    <div>Home</div>
+    <>
+      {/* <div>Home</div> */}
+      <Divider orientation="left">Home</Divider>
+    </>
   )
 }
