184927e73b67b277cd9d4e920ade51545c3c643d
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 14:08:52 +0330

    change header nav links color

--- src/components/Link/NavLink.tsx
+++ src/components/Link/NavLink.tsx
@@ -8,9 +8,9 @@ export const NavLink = styled(Link)`
     color: LightGray ;
   };
   &:focus{
-      color: DarkSlateGray  ;
+      color: white  ;
   };
   &:active{
-      color: DarkSlateGray  ;
+      color: white  ;
   };
 `
