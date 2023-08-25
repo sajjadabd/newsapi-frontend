13e8e3e426f5a502294ca534c6a7d69b030cadfe
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 21:34:48 +0330

    design profile with antd

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,89 @@
+import { Select, Space } from 'antd';
+import type { SelectProps } from 'antd';
+import { Button, Form, Input, Radio } from 'antd';
 
+const options: SelectProps['options'] = [];
+type LayoutType = Parameters<typeof Form>[0]['layout'];
+
+for (let i = 10; i < 36; i++) {
+  options.push({
+    label: i.toString(36) + i,
+    value: i.toString(36) + i,
+  });
+}
+
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 16 },
+};
+
+
+const handleChange = (value: string[]) => {
+  console.log(`selected ${value}`);
+};
 
 export default function Profile () {
     return (
-      <div>Profile</div>
+      <>
+        <div>Profile</div>
+        <Form
+        {...layout}
+        style={{ maxWidth: 600 }}
+        >
+          <Form.Item label="Sources">
+            <Select
+              mode="multiple"
+              allowClear
+              style={{ width: '100%' }}
+              placeholder="Please select"
+              defaultValue={['a10', 'c12']}
+              onChange={handleChange}
+              options={options}
+            />
+          </Form.Item>
+          <Form.Item label="Categories">
+            <Select
+              mode="multiple"
+              allowClear
+              style={{ width: '100%' }}
+              placeholder="Please select"
+              defaultValue={['a10', 'c12']}
+              onChange={handleChange}
+              options={options}
+            />
+          </Form.Item>
+          <Form.Item label="Countries">
+            <Select
+              mode="multiple"
+              allowClear
+              style={{ width: '100%' }}
+              placeholder="Please select"
+              defaultValue={['a10', 'c12']}
+              onChange={handleChange}
+              options={options}
+            />
+          </Form.Item>
+          <Form.Item label="Authors">
+            <Select
+              mode="multiple"
+              allowClear
+              style={{ width: '100%' }}
+              placeholder="Please select"
+              defaultValue={['a10', 'c12']}
+              onChange={handleChange}
+              options={options}
+            />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" >Submit</Button>
+          </Form.Item>
+        </Form>
+      </>
     )
   }
   
