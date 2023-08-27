199a51157e5ffc2d47ed02cedbe769d8b6eb7388
sajjadabd <mynameissajjad@gmail.com>
2023-08-27 12:52:33 +0330

    clear comments and unused imports in Register.tsx

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,11 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {StyledForm} from '../../components/Form/StyledForm';
-// import Input from '../../components/Form/Input';
-// import Button from '../../components/Form/Button';
 import { registerRequestURL } from '../../services/api';
-
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Button, Input, Space } from 'antd';
 import { Center } from '../../components/Center/Center';
@@ -33,10 +29,7 @@ function Register() {
   };
 
   const handleSubmit = async () => {
-    //event.preventDefault();
     setSubmitFormLoading(true);
-    console.log('registered...');
-    // Simulate a successful registration
     
     try {
       const response = await axios.post( registerRequestURL , { 
@@ -45,13 +38,10 @@ function Register() {
         password : formData.password,
       })
 
-      console.log(response.data)
-
       const { access_token } = response.data;
 
       localStorage.setItem('access_token', access_token);
 
-      // Redirect the user to the home page or another appropriate page
       window.location.href = '/';
 
     } catch (error) {
@@ -74,8 +64,6 @@ function Register() {
       <h2>Registration Page</h2>
       <Space direction="vertical">
 
-          {/* <label htmlFor="username">Username</label> */}
-
           <Input
             type="text"
             id="username"
@@ -86,10 +74,6 @@ function Register() {
             autoComplete="off"
           />
 
-
-
-          {/* <label htmlFor="email">Email</label> */}
-
           <Input
             type="email"
             id="email"
@@ -99,9 +83,7 @@ function Register() {
             onChange={handleInputChange}
           />
 
-          {/* <label htmlFor="password">Password</label> */}
 
-          
           <Input.Password
             placeholder="password"
             name="password"
@@ -111,13 +93,14 @@ function Register() {
           />
           
 
-
           <Button 
           loading={submitFormLoading}
           style={{ width: '100%' }} 
           onClick={() => handleSubmit()}>
             Register
           </Button>
+
+          
         </Space>
 
       <Space style={{ marginTop : '20px' }} direction="horizontal">
